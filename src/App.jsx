61e3e7a066f1db48a4ffdd584ea75c633eb01338
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-04 01:30:27 -0300

    setting provider to use global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-function App() {
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import UsersContainer from './components/UsersContainer';
+
+import './styles/global.scss'
+
+const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Provider store={store}>
+      <Header/>,
+      <UsersContainer/>
+      {/* <Dashboard/> */}
+    </Provider>
   );
 }
 
