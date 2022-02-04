b2cc834c4bc7e87e679dc80f7885513468d7d290
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-04 01:32:25 -0300

    creating header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/header.scss'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
