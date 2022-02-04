65e99011fe338331ed9014c54c8332348d5c3cad
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-04 01:33:05 -0300

    creating dashboard component

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+// import Header from '../components/Header';
+// // import UsersContainer from '../components/UsersContainer';
+
+// const Dashboard = () => {
+//     return (
+//         <Header/>,
+//         <UsersContainer/>
+//     );
+// }
+
+// export default Dashboard;
