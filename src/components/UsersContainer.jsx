5f1b6ed4477b244e5ec424ccd55ab53dbb851b0c
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-04 01:32:47 -0300

    creating UsersContainer component

---
+++ src/components/UsersContainer.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react'
+
+
+import '../styles/users.scss'
+
+// import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+
+const UsersContainer = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data')
+        .then(response => response.json())
+        .then(json => setUsers(json))
+    }, [])
+
+    // function handleCreateNewTask() {
+    //     // 
+    // }
+
+    // function handleToggleTaskCompletion() {
+    //     //  
+    // }
+
+    // function handleRemoveTask() {
+    //     // 
+    // }
+
+    return (
+        <section className="task-list container">
+            <header>
+                <h2>Users List</h2>
+
+                <div className="input-group">
+                    {/* <input type="text" placeholder="Adicionar novo todo"/> */}
+                    <button type="submit" data-testid="add-task-button" >
+                        Add new
+                    </button>
+                </div>
+            </header>
+
+            <main>
+                {/* <ul>
+                    <li>
+                        <div>
+                            <label className="checkbox-container">
+                                <input type="checkbox" readOnly />
+                                <span className="checkmark"></span>
+                            </label>
+                            <p></p>
+                        </div>
+
+                        <button type="button" data-testid="remove-task-button">
+                            <FiTrash size={16} />
+                        </button>
+                    </li>
+                </ul> */}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>E-mail</th>
+                            <th>City</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.username}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.address.city}</td>
+                                    <td align="center" className="button-column">
+                                        <button type="submit" style={{backgroundColor: "orange"}}>
+                                            Edit
+                                        </button>
+                                        
+                                    </td>
+                                    <td align="center" className="button-column">
+                                        <button type="submit" style={{backgroundColor: "red"}}>
+                                        Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </main>
+        </section>
+    )
+}
+
+export default UsersContainer;
