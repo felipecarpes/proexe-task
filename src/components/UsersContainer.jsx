da12579274e20ce1ebfc722dd2494701050e3aad
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-05 17:23:25 -0300

    adjust user container component

--- src/components/UsersContainer.jsx
+++ src/components/UsersContainer.jsx
@@ -1,101 +1,182 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect, useCallback } from 'react';
+import { useDispatch, connect } from 'react-redux';
+import { addNewUserRequest, removeUserRequest, editUserRequest } from '../store/modules/user/actions';
+
+import DeleteModal from './DeleteModal';
 
 import '../styles/users.scss'
-
-// import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import '../styles/form.scss'
 
 const UsersContainer = () => {
+    const [users, setUsers] = useState([]);
+    const [addUser, setAddUser] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [userId, setUserId] = useState('');
+    const [user, setUser] = useState({ 
+        name: '',
+        email: ''
+    })
 
-    const [users, setUsers] = useState([])
-
+    const dispatch = useDispatch()
+    
     useEffect(() => {
         fetch('https://my-json-server.typicode.com/karolkproexe/jsonplaceholderdb/data')
         .then(response => response.json())
-        .then(json => setUsers(json))
+        .then(json => {
+            setUsers(json)
+        })
     }, [])
 
-    // function handleCreateNewTask() {
-    //     // 
-    // }
+    const handleOpenModal = (id) => {
+        setUserId(id)
+        document.getElementsByClassName('modal')[0].style.display = 'flex'
+    }
 
-    // function handleToggleTaskCompletion() {
-    //     //  
-    // }
+    const handleCreateNewUser = useCallback(() => {
+        dispatch(addNewUserRequest(users, user))
+        setTimeout(() => setAddUser(false), 1000)
+    }, [dispatch, users, user])
 
-    // function handleRemoveTask() {
-    //     // 
-    // }
+    const handleRemoveUser = () => {
+        dispatch(removeUserRequest(users, userId))
+    }
+
+    const handleEditUser = useCallback(() => {
+        dispatch(editUserRequest(users, user))
+        setTimeout(() => setEdit(false), 1000)
+    }, [dispatch, user, users])
+
+    const handleEdit = (user) => { 
+        setEdit(true)
+        setUser({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }
 
     return (
-        <section className="task-list container">
-            <header>
-                <h2>Users List</h2>
-
-                <div className="input-group">
-                    {/* <input type="text" placeholder="Adicionar novo todo"/> */}
-                    
-                    <Link to="/add-user" className="add-new-button">
-                        Add new
-                    </Link>
-                </div>
-            </header>
-
-            <main>
-                {/* <ul>
-                    <li>
-                        <div>
-                            <label className="checkbox-container">
-                                <input type="checkbox" readOnly />
-                                <span className="checkmark"></span>
-                            </label>
-                            <p></p>
-                        </div>
-
-                        <button type="button" data-testid="remove-task-button">
-                            <FiTrash size={16} />
-                        </button>
-                    </li>
-                </ul> */}
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Username</th>
-                            <th>E-mail</th>
-                            <th>City</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map(item => (
-                                <tr key={item.id}>
-                                    <td>{item.id}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.username}</td>
-                                    <td>{item.email}</td>
-                                    <td>{item.address.city}</td>
-                                    <td align="center" className="button-column">
-                                        <button type="submit" style={{backgroundColor: "orange"}}>
-                                            edit
-                                        </button>
-                                        
-                                    </td>
-                                    <td align="center" className="button-column">
-                                        <button type="submit" style={{backgroundColor: "red"}}>
-                                            delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </main>
-        </section>
+        <>
+            {
+                addUser ? (
+                    <section className="form container">
+                        <header>
+                            <h2>Form</h2>
+                        </header>
+                        <main>
+                            <div className="form-body">
+                                <div className="input-container">
+                                    <div className="input-row">
+                                        <label htmlFor="name">Name</label>
+                                        <input id="name" type="text" onChange={(e) => setUser({ ...user, name: e.target.value })} placeholder="type the name"/> 
+                                        {/* { validate.name ? <span >Type a valid name to continue!</span> : '' } */}
+                                    </div>
+                                    <div className="input-row">
+                                        <label htmlFor="email">E-mail</label>
+                                        <input id="email" type="text" onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="type the e-mail"/>
+                                        {/* { validate.email ? <span >Type a valid e-mail to continue!</span> : '' } */}
+                                    </div>
+                                </div>
+                                <div className="send-container">
+                                    <button className="cancel-button" onClick={() => setAddUser(false)}>
+                                        Cancel
+                                    </button>
+                                    <button type="submit" className="submit-button" onClick={handleCreateNewUser}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </main>
+                    </section>
+                ) : edit ? (
+                    <section className="form container">
+                        <header>
+                            <h2>Form</h2>
+                        </header>
+                        <main>
+                            <div className="form-body">
+                                <div className="input-container">
+                                    <div className="input-row">
+                                        <label htmlFor="name">Name</label>
+                                        <input id="name" type="text" value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} placeholder="type the name"/> 
+                                        {/* { validate.name ? <span >Type a valid name to continue!</span> : '' } */}
+                                    </div>
+                                    <div className="input-row">
+                                        <label htmlFor="email">E-mail</label>
+                                        <input id="email" type="text" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="type the e-mail"/>
+                                        {/* { validate.email ? <span >Type a valid e-mail to continue!</span> : '' } */}
+                                    </div>
+                                </div>
+                                <div className="send-container">
+                                    <button className="cancel-button" onClick={() => setEdit(false)}>
+                                        Cancel
+                                    </button>
+                                    <button type="submit" className="submit-button" onClick={handleEditUser}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </main>
+                    </section>
+                ) : (
+                    <section className="user-list container">
+                        <header>
+                            <h2>Users List</h2>
+            
+                            <div className="input-group">
+                                <button className="add-new-button" onClick={() => setAddUser(true)}> 
+                                    Add new
+                                </button>
+                            </div>
+                        </header>
+                        <main>
+                            {   users.length === 0 ? <span>There are no user records.</span> :
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Id</th>
+                                            <th>Name</th>
+                                            <th>Username</th>
+                                            <th>E-mail</th>
+                                            <th>City</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            users.map(item => (
+                                                <>
+                                                
+                                                <tr key={item.id}>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.name}</td>
+                                                    <td>{item.username}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.address.city}</td>
+                                                    <td align="center" className="button-column">
+                                                        <button type="submit" style={{backgroundColor: "orange"}} onClick={() => handleEdit(item)}>
+                                                            edit
+                                                        </button>
+                                                    </td>
+                                                    <td align="center" className="button-column">
+                                                        <button type="submit" style={{backgroundColor: "red"}} onClick={() => handleOpenModal(item.id)}>
+                                                            delete
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                                </>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
+                            }
+                        </main>
+                    </section>
+                    ) 
+                }
+            <DeleteModal deleteUser={() => handleRemoveUser()}/>
+        </>
     )
 }
 
