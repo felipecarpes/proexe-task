538173986f7c326f4012ba92e1b43937a128bf98
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-04 22:36:19 -0300

    adjust AddUser page

---
+++ src/pages/AddUser.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import AddUserForm from '../components/AddUserForm';
+
+import '../styles/global.scss';
+
+const AddUser = () => {
+    return (
+        <>
+            <Header/>
+            <AddUserForm/>
+        </>
+    );
+}
+
+export default AddUser;
