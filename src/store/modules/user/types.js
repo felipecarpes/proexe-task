a03113922741f12a0d7ffbf2f7728751225598f8
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-05 17:24:58 -0300

    setting types

--- src/store/modules/user/types.js
+++ src/store/modules/user/types.js
@@ -1,9 +1,7 @@
-
 const ActionTypes = {
-    addProductToCartRequest: 'ADD_PRODUCT_TO_CART_REQUEST',
-    addProductToCartSuccess: 'ADD_PRODUCT_TO_CART_SUCCESS',
-    addProductToCartFailure: 'ADD_PRODUCT_TO_CART_FAILURE',
+    addNewUserRequest: 'ADD_NEW_USER_REQUEST',
+    removeUserRequest: 'REMOVE_USER_REQUEST',
+    editUserRequest: 'EDIT_USER_REQUEST'
 }
 
 export default ActionTypes
-    
